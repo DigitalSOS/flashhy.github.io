4643d2447c5ffd22206201641d6d4b7d3882ec01
Fouad <farayssi79@gmail.com>
2024-09-07 19:15:00 -0700

    flashhy1.3

---
+++ JS/home_button.js
@@ -0,0 +1,11 @@
+// Home button click animation
+document.getElementById('home-button').addEventListener('click', function() {
+    const homeIcon = this.querySelector('img');
+    homeIcon.classList.add('clicked');
+    setTimeout(() => {
+        homeIcon.classList.remove('clicked');
+        window.location.href = '/home_page.html'; // Redirect to home after animation
+    }, 300);
+});
+
+
